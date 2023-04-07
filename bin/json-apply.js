af1990ab66f8e40fa4334b72a9b5926d998041c9
maxlath <k@maxlath.eu>
2023-04-07 20:01:53 +0200

    adapt code from ndjson-apply

---
+++ bin/json-apply.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+import { readFile, writeFile } from 'fs/promises'
+import path from 'node:path'
+import { program } from 'commander'
+import { detectIndentation } from '../lib/utils.js'
+import diff from '../lib/diff.js'
+
+const packageJson = await readFile(new URL('../package.json', import.meta.url))
+const { version, description } = JSON.parse(packageJson)
+
+program
+.description(description)
+.arguments('<js-function-module> <json-files-paths...>')
+.option('-d, --diff', 'Preview the changes between the input and the transformation output')
+.option('-i, --in-place', 'Edit files in place')
+.version(version)
+// .addHelpText('after', helpText)
+
+program.parse(process.argv)
+
+let [ fnModulePath, ...jsonFilesPaths ] = program.args
+
+const { diff: showDiff, inPlace } = program.opts()
+
+function exit (errorMessage) {
+  console.error(errorMessage)
+  process.exit(1)
+}
+
+if (!fnModulePath) program.help()
+
+let functionName
+[ fnModulePath, functionName = 'default' ] = fnModulePath.split('#')
+
+const resolvedPath = path.resolve(fnModulePath)
+let exports
+try {
+  exports = await import(resolvedPath)
+} catch (err) {
+  if (err.code === 'ERR_MODULE_NOT_FOUND') {
+    exit(`function module not found: ${fnModulePath}`)
+  } else {
+    throw err
+  }
+}
+
+if (jsonFilesPaths.length === 0) {
+  exit(`expected to get a path to at least one json file`)
+}
+
+let transformFn
+transformFn = exports[functionName] || exports.default?.[functionName]
+if (transformFn == null) {
+  exit(`function not found: ${functionName} (found: ${Object.keys(exports)})`)
+}
+
+if (typeof transformFn !== 'function') {
+  const context = { resolvedPath, exports: Object.keys(exports), rawAdditionalArgs, additionalArgs }
+  exit(`transform function not found\n${JSON.stringify(context, null, 2)}`)
+}
+
+for (const jsonFilePath of jsonFilesPaths) {
+  const absolutePath = path.resolve(process.cwd(), jsonFilePath)
+  const json = await readFile(absolutePath, 'utf8')
+  const indentation = detectIndentation(json)
+  const obj = JSON.parse(json)
+  const transformedObj = await transformFn(obj)
+  if (showDiff) {
+    process.stdout.write((diff(obj, transformedObj)) + '\n')
+  } else {
+    const rejsonified = JSON.stringify(transformedObj, null, indentation)
+    if (inPlace) {
+      await writeFile(absolutePath, rejsonified)
+    } else {
+      process.stdout.write((rejsonified) + '\n')
+    }
+  }
+}
